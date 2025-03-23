ed6c466e83a2020013d6a56e9c3c97f9bf12f737
Tobias Malcherek <tobias.malcherek@sti.valantic.com>
2025-03-23 18:36:07 +0100

    -

--- Test/run.sh
+++ Test/run.sh
@@ -1,5 +1,22 @@
 #!/usr/bin/with-contenv bashio
 
-echo "Hello world!"
+set +x
 
-python3 -m http.server 8000
+# Create main config
+CLIENTID=$(bashio::config 'upstreamhost')
+CLIENTSECRET=$(bashio::config 'upstreamport')
+ROOM=$(bashio::config 'listenport')
+
+echo "Preparing to run sonos-kids"
+echo "CLIENTID: $CLIENTID"
+echo "CLIENTSECRET: $CLIENTSECRET"
+echo "ROOM: $ROOM"
+
+sed -i "s/__CLIENTID__/${CLIENTID}/g" /app/Sonos-Kids-Controller/server/config/config.json
+sed -i "s/__CLIENTSECRET__/${CLIENTSECRET}/g" /app/Sonos-Kids-Controller/server/config/config.json 
+sed -i "s/__ROOM__/${ROOM}/g" /app/Sonos-Kids-Controller/server/config/config.json
+
+echo "Generated Config"
+cat /app/Sonos-Kids-Controller/server/config/ 
+
+npm start
