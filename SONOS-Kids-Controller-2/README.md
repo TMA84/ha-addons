b203201e37175ece76ceaef146ba4dd26a29088e
Tobias Malcherek <tobias.malcherek@sti.valantic.com>
2025-03-23 21:04:37 +0100

    -

---
+++ SONOS-Kids-Controller-2/README.md
@@ -0,0 +1,19 @@
+# Home Assistant Addon for Sonos-Kids-Controller
+## Sonos-Kids-Controller
+Description of project see https://github.com/Thyraz/Sonos-Kids-Controller
+
+## About Home Assistant Addon Sonos-Kids-Controller
+This add-on is a port for Home Assistant from https://github.com/Thyraz/Sonos-Kids-Controller
+
+## Overview
+Sonos-Kids-Controller is software designed to create a touch-based jukebox for kids. It supports Spotify streaming and uses Sonos for audio output. This project enables children to interact with a user-friendly interface to select and play their favorite music or stories.
+
+## Features
+Touchscreen Jukebox: Designed for kids to easily navigate and play music.
+Spotify Integration: Supports Spotify Premium for seamless streaming.
+Sonos Audio Output: Utilizes Sonos speakers for high-quality sound.
+Text-to-Speech (TTS): Clickable text in the UI helps kids identify episodes by name.
+Multi-Service Support: Compatible with local Sonos library, Spotify, Apple Music, and Amazon Music.
+
+## Dependencies
+In addition to this add-on, you must also install the SONOS-API add-on.
---
+++ SONOS-Kids-Controller-2/run.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/with-contenv bashio
+
+set +x
+
+# Create main config
+CLIENTID=$(bashio::config 'clientid')
+CLIENTSECRET=$(bashio::config 'clientsecret')
+ROOM=$(bashio::config 'room')
+SERVER=$(bashio::config 'apiserver')
+PORT=$(bashio::config 'apiport')
+
+echo "Preparing to run sonos-kids"
+echo "CLIENTID: $CLIENTID"
+echo "CLIENTSECRET: $CLIENTSECRET"
+echo "ROOM: $ROOM"
+
+sed -i "s/__CLIENTID__/${CLIENTID}/g" /app/Sonos-Kids-Controller/server/config/config.json
+sed -i "s/__CLIENTSECRET__/${CLIENTSECRET}/g" /app/Sonos-Kids-Controller/server/config/config.json 
+sed -i "s/__ROOM__/${ROOM}/g" /app/Sonos-Kids-Controller/server/config/config.json
+sed -i "s/__SERVER__/${SERVER}/g" /app/Sonos-Kids-Controller/server/config/config.json
+sed -i "s/__PORT__/${PORT}/g" /app/Sonos-Kids-Controller/server/config/config.json
+
+ln -s $(bashio::config 'data') /app/Sonos-Kids-Controller/server/config/data.json
+
+echo -e "\nGenerated Config"
+cat /app/Sonos-Kids-Controller/server/config/config.json
+echo -e "\n"
+
+echo -e "\nData"
+cat /app/Sonos-Kids-Controller/server/config/data.json
+echo -e "\n"
+
+npm start
