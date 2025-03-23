47588ede893c02ad74c8ac6039e769f0d36812d9
Tobias Malcherek <tobias.malcherek@sti.valantic.com>
2025-03-23 19:58:46 +0100

    -

--- Test/run.sh
+++ Test/run.sh
@@ -6,6 +6,8 @@ set +x
 CLIENTID=$(bashio::config 'clientid')
 CLIENTSECRET=$(bashio::config 'clientsecret')
 ROOM=$(bashio::config 'room')
+SERVER=$(bashio::config 'apiserver')
+PORT=$(bashio::config 'apiport')
 
 echo "Preparing to run sonos-kids"
 echo "CLIENTID: $CLIENTID"
@@ -15,6 +17,10 @@ echo "ROOM: $ROOM"
 sed -i "s/__CLIENTID__/${CLIENTID}/g" /app/Sonos-Kids-Controller/server/config/config.json
 sed -i "s/__CLIENTSECRET__/${CLIENTSECRET}/g" /app/Sonos-Kids-Controller/server/config/config.json 
 sed -i "s/__ROOM__/${ROOM}/g" /app/Sonos-Kids-Controller/server/config/config.json
+sed -i "s/__SERVER__/${SERVER}/g" /app/Sonos-Kids-Controller/server/config/config.json
+sed -i "s/__PORT__/${PORT}/g" /app/Sonos-Kids-Controller/server/config/config.json
+
+ln -s $(bashio::config 'data') /app/Sonos-Kids-Controller/server/config/data.json
 
 echo "Generated Config"
 cat /app/Sonos-Kids-Controller/server/config/config.json
